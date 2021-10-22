88c11c7e3a3ac527ac317783b612fe58233afbb9
Kumie-M <49703108+Kumie-M@users.noreply.github.com>
2021-10-22 14:22:50 +0900

    新規プロジェクト作成

---
+++ SampleCustomView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleCustomView
+//
+//  Created by Kumie Miyata on 2021/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
